edf94dcb2778f2a66d574789e8f9a1d6167b93b1
Prasanna Kulkarni <prasanna.kulkarni.btech2020@sitpune.edu.in>
2023-04-22 21:20:54 +0530

    Booking event page_organising specific eventpage

--- frontend/src/views/BookPage.vue
+++ frontend/src/views/BookPage.vue
@@ -1,15 +1,244 @@
 <template>
-    <div>
-
-    </div>
+    <div class="container">
+  <div class="row header">
+    <h1>Book Event &nbsp;</h1>
+    <h3>Fill out the form below!</h3>
+  </div>
+  <div class="row body">
+    <form action="#">
+      <ul>
+        
+        <li>
+          <p class="left">
+            <label for="first_name">First name</label>
+            <input type="text" name="first_name" placeholder="John" />
+          </p>
+          <p class="pull-right">
+            <label for="last_name">Last name</label>
+            <input type="text" name="last_name" placeholder="Smith" />      
+          </p>
+        </li>
+        
+        <li>
+          <p>
+            <label for="email">Email <span class="req">*</span></label>
+            <input type="email" name="email" placeholder="john.smith@gmail.com" />
+          </p>
+        </li>        
+        <li><div class="divider"></div></li>
+        <li>
+          <label for="comments">Comments</label>
+          <textarea cols="46" rows="3" name="comments"></textarea>
+        </li>
+        <!-- Add here more inputs -->
+        
+        <li>
+          <input class="btn btn-submit" type="submit" value="Submit" />
+          <small>or press <strong>enter</strong></small>
+        </li>
+        
+      </ul>
+    </form>  
+  </div>
+</div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+    
+}
 </script>
 
 <style lang="scss" scoped>
+
+
+// Font imports
+
+@import url(https://fonts.googleapis.com/css?family=Lato:300,400,700);
+
+// Color vars
+
+$white: #fff;
+$grey: #ccc;
+$dark_grey: #555;
+$blue: #4f6fad;
+$pink: #ee9cb4;
+
+// Mixins
+
+@mixin lato-book { font-family: 'Lato', sans-serif; font-weight: 300; }
+@mixin lato-reg { font-family: 'Lato', sans-serif; font-weight: 400; }
+@mixin lato-bold { font-family: 'Lato', sans-serif; font-weight: 700; }
+@mixin btn($color) {
+  background-color: $color;
+  border-bottom-color: darken($color, 15%);
+  &:hover {
+      background-color: lighten($color, 5%);
+    }
+}
+
+// Functions
+
+@function pxtoem($target, $context){
+  @return ($target/$context)+0em;
+}
+
+//
+
+body {
+  background-color: lighten($grey, 10%);
+  font-size: 100%;
+  @include lato-reg;
+}
+
+.container {  
+  max-width: 510px;
+  min-width: 324px;
+  margin: 50px auto 0px;
+  background-color: $white;
+  border: 1px solid lighten($grey, 1%); 
+  border-bottom: 3px solid $grey;
+}
+.row {
+  width: 100%;
+  margin: 0 0 1em 0;
+  padding: 0 2.5em;
+  &.header {
+    padding: 1.5em 2.5em;
+    border-bottom: 1px solid $grey; 
+    // Yaha pe bg image change karna
+    background: url("https://www.specialevents.com/sites/specialevents.com/files/styles/article_featured_retina/public/gallery_promo_image/InVision_Shaklee_Global_Live.jpg?itok=huOoiSZJ") left -80px;
+    color: $white;
+  }
+  &.body {
+    padding: .5em 2.5em 1em;
+  }
+}
+.pull-right {
+  float: left; 
+}
+h1 {
+  @include lato-book;
+  display: inline-block;
+  font-weight: 100;
+  font-size: 3.8125em;
+  border-bottom: 1px solid hsla(100%, 100%, 100%, 0.3);
+  margin: 0 0 0.1em 0;
+  padding: 0 0 0.4em 0;
+}
+h3 {
+  @include lato-reg;
+  font-size: pxtoem(20, 16);
+  margin: 1em 0 0.4em 0;
+}
+.btn {
+  font-size: pxtoem(17, 16);
+  display: inline-block;
+  padding: 0.74em 1.5em;
+  margin: 1.5em 0 0;
+  color: $white;
+  border-width: 0 0 0 0;
+  border-bottom: 5px solid;
+  text-transform: uppercase;
+  @include btn(darken($grey, 10%));
+  @include lato-book;
+  &.btn-submit {
+    @include btn($blue);
+  }
+}
+
+form {
+  max-width: 100%;
+  display: block;
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    li {
+      margin: 0 0 0.25em 0; 
+      clear: both;
+      display: inline-block;
+      width: 100%;
+      &:last-child {
+          margin: 0;    
+        }
+        p {
+          margin: 0;
+          padding: 0;
+          float: left;
+          &.right {
+            float: right;
+          }
+        }     
+        .divider {
+          margin: 0.5em 0 0.5em 0;
+          border: 0;
+          height: 1px;
+          width: 100%;
+          display: block;
+          background-color: $blue;
+          background-image: linear-gradient(to right, $pink, $blue);
+        }
+        .req {
+          color: $pink; 
+        }
+    }
+  }
+  label {
+    display: block;
+    margin: 0 0 0.5em 0;
+    color: $blue;
+    font-size: pxtoem(16, 16);
+  }
+  input {
+    margin: 0 0 0.5em 0;
+    border: 1px solid $grey;
+    padding: 6px 10px;
+    color: $dark_grey;
+    font-size: pxtoem(16, 16);
+  }
+  textarea {
+    border: 1px solid $grey;
+    padding: 6px 10px;
+    width: 100%;
+    color: $dark_grey;
+  }
+  small {
+    color: $blue;
+    margin: 0 0 0 0.5em;
+  }
+}
+
+// Media Queries
+
+@media only screen and (max-width:480px) {
+  .pull-right {
+    float: none; 
+  }
+  input {
+    width: 100%; 
+  }
+  label {
+    width: 100%;
+    display: inline-block;
+
+    clear: both;
+  }
+  li, p {
+   width: 100%; 
+  }
+  input.btn {
+   margin: 1.5em 0 0.5em; 
+  }
+  h1 {
+   font-size: pxtoem(36, 16); 
+  }
+  h3 {
+    font-size: pxtoem(18, 16)
+  }
+  li small {
+   display: none; 
+  }
+}
 
 </style>
--- frontend/src/views/OrgForm.vue
+++ frontend/src/views/OrgForm.vue
@@ -1,7 +1,9 @@
 <template>
+  <div class="whole-page">
     <div class="form-container">
       <h3>Please fill in the following details</h3>
       <form @submit.prevent="submitForm">
+        
         <div class="form-group">
           <label for="name">Name</label>
           <input type="text" id="name" v-model="formData.name" />
@@ -22,9 +24,14 @@
           <label for="event-city">Event City</label>
           <input type="text" id="event-city" v-model="formData.eventCity" />
         </div>
+        <div class="form-group">
+          <label for="event-desc">Event Description</label>
+          <input class="form-group-desc" type="text" id="event-desc" height="300" v-model="formData.eventdesc" />
+        </div>
         <button type="submit">Submit</button>
       </form>
     </div>
+  </div>
   </template>
   
   <script>
@@ -36,7 +43,8 @@
           email: '',
           phone: '',
           eventRegion: '',
-          eventCity: ''
+          eventCity: '',
+          eventdesc:''
         }
       };
     },
@@ -52,22 +60,32 @@
   
   <style scoped>
 
+.whole-page {
+    background: linear-gradient(#395487, #243b55);
+    height: 100%;
+}
+
 h3 {
     font-size: 21px;
     position: relative;
     left: 37px;
+    color: white;
+    top: 81px;
+    
+
 }
 
 .form-container {
     max-width: 400px;
     margin: 0 auto;
+   
   }
   
   form {
     display: flex;
     flex-direction: column;
     gap: 1rem;
-    margin-top: 28px;
+    margin-top: 128px;
 
 
   }
@@ -75,30 +93,53 @@ h3 {
   .form-group {
     display: flex;
     flex-direction: column;
+
+  }
+  .form-group-desc{
+    display: flex;
+    flex-direction: column;
+    height:200px;
   }
   
   label {
     font-weight: bold;
     margin-bottom: 0.5rem;
+    color: white;
   }
   
   input {
     padding: 0.5rem;
     border: 1px solid #ccc;
     border-radius: 4px;
+    color: white;
   }
   
   button[type="submit"] {
-    padding: 0.5rem;
+    /* padding: 0.5rem;
     background-color: #007bff;
     color: #fff;
     border: none;
     border-radius: 4px;
+    cursor: pointer; */
+    
+    width: 133px;
+    height: 46px;
+    margin: 19px 174px;
+    border-radius: 10px;
     cursor: pointer;
+    background: #03e9f4;
+    box-shadow: inset 0 0 0 #393131;
+    transition: ease-out 0.8s;
+    outline: none;
+    position: relative;
+    right: 172px;
   }
   
   button[type="submit"]:hover {
-    background-color: #0056b3;
+    /* background-color: #06d8e3; */
+    /* color: #03d1dc; */
+    box-shadow: inset 300px 0 0 0 #03e9f4;
+
   }
   </style>
   
